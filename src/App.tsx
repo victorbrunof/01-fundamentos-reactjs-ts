490c652a23a6f0eca87debd039d5975dfcb49e19
Victor Bruno <victorbrunof@icloud.com>
2022-08-09 21:08:39 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { Header } from './components/Header/Header';
+import { Post } from './components/Post/Post'
+import { Sidebar } from './components/Sidebar/Sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/victorbrunof.png',
+      name: 'Victor Bruno',
+      role: 'Developer',
+    },
+    content: [
+      { type: 'pharagraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-08-04 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/victorbrunof.png',
+      name: 'Bruno Fernandes',
+      role: 'Developer',
+    },
+    content: [
+      { type: 'pharagraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-08-5 20:00:00'),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post 
+                key = {post.id}
+                author = {post.author}
+                content = {post.content}
+                publishedAt = {post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
