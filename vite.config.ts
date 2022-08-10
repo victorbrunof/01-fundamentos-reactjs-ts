067f14dce76c9d29a44a84b1edd3cba2bd10cece
Victor Bruno <victorbrunof@icloud.com>
2022-08-09 23:40:35 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/01-fundamentos-reactjs-ts',
   plugins: [react()],
   server: {
     port: 3000,
